a03b05561ee2b84c432ae075d70a0310d5ce3105
FabiolaSaga <saga.fabiola@gmail.com>
2019-02-26 20:09:41 -0600

    Added checkQuestion function Added nextQuestion function

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -8,7 +8,9 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -18,11 +20,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag ==  1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        questionNumber =  questionNumber + 1
+        nextQuestion()
     }
     
     
@@ -32,12 +45,23 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
-        
+        if questionNumber <= 12 {
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+        } else {
+            print("End of Quiz")
+            questionNumber = 0
+        }
     }
     
     
     func checkAnswer() {
         
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        if correctAnswer == pickedAnswer {
+            print("You got it!")
+        } else {
+            print("Wrong")
+        }
     }
     
     
